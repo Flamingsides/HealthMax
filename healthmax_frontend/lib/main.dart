d2a888dbbe7fbaf101ce2957b3c2665b0ef3e06e
Zuberi, Suhaib <sz2056@hw.ac.uk>
2026-02-21 14:12:39 +0800

    Initialised folders for backend and api teams to upload their code

--- healthmax_frontend/lib/main.dart
+++ healthmax_frontend/lib/main.dart
@@ -21,6 +21,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'HealthMax',
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
