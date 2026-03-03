e8833dd4c10b4b07aaa59c11c168e5f6888cfefe
Zuberi, Suhaib <sz2056@hw.ac.uk>
2026-03-03 11:12:05 +0800

    Added cards to post registration page for user. Need to work on slider next

--- healthmax_frontend/lib/UserPages/registration_intro.dart
+++ healthmax_frontend/lib/UserPages/registration_intro.dart
@@ -1,10 +1,92 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import '../GeneralPages/helper_widgets.dart';
 
 class RegistrationIntro extends StatelessWidget {
   const RegistrationIntro({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Screen(
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Positioned(
+            top: 80,
+            left: 0,
+            right: 0,
+            child: Text(
+              "Start your Wellness Journey",
+              style: TextStyle(
+                fontFamily: "LexendExaNormal",
+                fontWeight: FontWeight.w900,
+                fontSize: 35,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Positioned(
+            left: 0,
+            top: 220,
+            child: SizedBox(
+              width: 200,
+              child: Image(
+                image: AssetImage("assets/images/registration-intro-move.png"),
+              ),
+            ),
+          ),
+          Positioned(
+            right: 0,
+            top: 240,
+            child: SizedBox(
+              width: 170,
+              child: Image(
+                image: AssetImage("assets/images/registration-intro-track.png"),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            top: 340,
+            child: SizedBox(
+              width: 143,
+              child: Image(
+                image: AssetImage("assets/images/registration-intro-rest.png"),
+              ),
+            ),
+          ),
+          Positioned(
+            right: 0,
+            top: 380,
+            child: SizedBox(
+              width: 235,
+              child: Image(
+                image: AssetImage(
+                  "assets/images/registration-intro-balance.png",
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            top: 440,
+            child: SizedBox(
+              width: 134,
+              child: Image(
+                image: AssetImage("assets/images/registration-intro-plan.png"),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 600,
+            left: 0,
+            right: 0,
+            child: Slider(value: 0, onChanged: (_) => {}),
+          ),
+        ],
+      ),
+    );
   }
 }
